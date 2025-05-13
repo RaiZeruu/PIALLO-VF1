6555e1570aeb7fe28dbc5a7dd8f7e4bc1ab2f956
cerdocebolla <lordofsuperium17@gmail.com>
2025-05-13 10:43:42 +0000

    Initial commit

---
+++ activateMessageCommits.bat
@@ -0,0 +1,22 @@
+local function.true 
+
+then askForChild.messages 
+
+else 
+
+askForChild.commits 
+            then printMultiple.print = true 
+            
+            then print ("The PIALLO Software is now running successfully, enjoy.")
+            if printMultiple.print = false 
+            then askForChild.commits (messages) = true 
+
+            else.PrintedBySoftware = print ("PIALLO Software has faced an issue. Contact PIALLO help staff, or restart your WiFi connection.")
+            if goUp.checkUp let checkForward else 
+
+            then endInstant.end
+            end)
+    end)
+endInstant.end
+
+end)
---
+++ infoRunner.bat
@@ -0,0 +1,15 @@
+if runner.Function = local 
+then askForChild = parent.WiFi = infoRunner
+     else.ifFindableWiFi = true 
+     then searchForParent = WiFi.findableMode = true 
+
+     else (ifFalse) = not 
+     then find = findableMode = supportedByTheClient.source = true 
+
+    or false = setTrueEverytime.unBlockUser = setMode.true (activeTrue(activateMode))
+
+    goUp.checkForChild = if true 
+    then askForWiFi.findable 
+
+    if goUp (checkUp(searchForWiFi)) = true then endInstant.end
+    end)
